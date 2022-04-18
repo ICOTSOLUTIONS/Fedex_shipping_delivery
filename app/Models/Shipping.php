9b9e548795764ff5d2942e9ec238ac1444ed3951
umermuk <umerwell11234@gmail.com>
2022-04-18 11:46:17 +0500

    working

--- app/Models/Shipping.php
+++ app/Models/Shipping.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Shipping extends Model
 {
     use HasFactory;
+    public function status()
+    {
+        return $this->belongsTo(LogStatus::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login',[\App\Http\Controllers\AuthController::class,'login'])->name('login');
 Route::post('/login-process',[\App\Http\Controllers\AuthController::class,'loginProcess'])->name('login.process');
 Route::post('/contact',[\App\Http\Controllers\AuthController::class,'contact'])->name('contact');
+Route::post('/shipping',[\App\Http\Controllers\ShippmentController::class,'shipping'])->name('shipping.process');
+Route::post('/tracking',[\App\Http\Controllers\ShippmentController::class,'tracking'])->name('tracking.process');
+Route::post('/log-status/{id}',[\App\Http\Controllers\ShippmentController::class,'logStatus'])->name('log.status');
+Route::get('/status-change/{id}',[\App\Http\Controllers\ShippmentController::class,'statusChange'])->name('status.change');
 
 Route::middleware('AdminCheck')->group(function () {
     Route::get('/dashboard',[\App\Http\Controllers\AuthController::class,'dashboard'])->name('dashboard');
